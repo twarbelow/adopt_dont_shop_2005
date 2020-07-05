205323b5592ff794919df98410f1760e72d1cb99
twarbelow <taija.warbelow@google.com>
2020-07-05 12:51:00 -0700

    complete user story 5

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -15,6 +15,16 @@ class SheltersController < ApplicationController
     redirect_to "/shelters"
   end
 
+  def edit
+    @shelter = Shelter.find(params[:id])
+  end
+
+  def update
+    @shelter = Shelter.find(params[:id])
+    @shelter.update(shelter_params)
+    redirect_to "/shelters/#{@shelter.id}"
+  end
+  
   private
   def shelter_params
     params.permit(:name, :address, :city, :state, :zip)
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get '/shelters/new', to: 'shelters#new'
   post '/shelters', to: 'shelters#create'
   get '/shelters/:id', to: 'shelters#show' # figure out how to restrict what it looks for as :id to numerical value
+  get '/shelters/:id/edit', to: 'shelters#edit'
+  patch '/shelters/:id', to: 'shelters#update'
 end
