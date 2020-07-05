9f8b67202cf0e428d45725c530ed6a46f7344f6b
twarbelow <taija.warbelow@google.com>
2020-07-04 22:35:48 -0700

    add test for create shelter

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -25,4 +25,33 @@ RSpec.describe "shelter index page", type: :feature do
     expect(page).to have_content(happy_tails.state)
     expect(page).to have_content(happy_tails.zip)
   end
+
+  it "lets a user add a shelter" do
+    visit "/shelters"
+
+    expect(page).to have_selector(:link_or_button, 'New Shelter')
+
+    click_on 'New Shelter'
+
+    expect(page).to have_current_path('/shelters/new')
+    expect(page).to have_selector(:link_or_button, 'Create Shelter')
+    expect(page).to have_field('Name')
+    expect(page).to have_field('Address')
+    expect(page).to have_field('City')
+    expect(page).to have_field('State')
+    expect(page).to have_field('Zip')
+
+    new = {name: 'Tiny Animal Rescue', address: '321 Furry Feet Blv', city: 'My City', state: 'My State', zip: '47449'}
+
+    fill_in 'Name', with: new[:name]
+    fill_in 'Address', with: new[:address]
+    fill_in 'City', with: new[:city]
+    fill_in 'State', with: new[:state]
+    fill_in 'Zip', with: new[:zip]
+
+    click_on 'Create Shelter'
+
+    expect(page).to have_current_path('/shelters')
+    expect(page).to have_content(new[:name])
+  end 
 end
